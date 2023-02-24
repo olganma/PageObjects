d0e7f7089e294d1c5aaf8cefc355742b2483c3c7
Maevskaya Olga <olganma@gmail.com>
2023-02-24 12:33:46 +0300

    Pages and test from lection added

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,20 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class MoneyTransferTest {
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV2() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+}
